a180e32d77fb7ae572556319c177e862672fbfa1
Omar Zapata <andresete.chaos@gmail.com>
2023-02-21 18:24:51 -0500

    condicionales

---
+++ codes/Unidad1/switch.cxx
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+int day = 1;
+
+switch (day) {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    default:
+        cout << "Invalid day" << endl;
+        break;
+}
+}
