9202dce6fbc09cc4c28a47dd81a225f825c27529
Omar Zapata <Omar.Zapata@cern.ch>
2023-02-21 21:07:48 -0500

    added bucles

---
+++ codes/Unidad1/while.cxx
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+int i = 1;
+    while (i <= 5) {
+        //recuerden que acá pueden controlar el flujo con break y continue.
+        cout << i << endl;
+        i++;
+    }
+}
