b07be4dda6bb9f6d7bd749b9353ed989699faa66
jmejiagu <jhovanny.andres.mejia.guisao@cern.ch>
2023-03-14 17:47:46 -0500

    unidad2 update

---
+++ Documents/Unidad2/Clase2/geometrica.cpp
@@ -0,0 +1,27 @@
+// programa para desplegar la serie geometrica
+// a + ar + ar 2 + ar 3 +  ̇  ̇  ̇ + ar n – 1   esto debe converger a 2   para a=1 y r=1/2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  double a, r;
+  int j;
+  double total;
+
+  a = 1.0; r = 0.5;
+  total = 0;
+  j = 0;
+  
+  for ( ; j<11 ; j++)
+    {
+      total = total + a*pow(r,j);
+    }
+
+  cout << "la suma de la seri geometrica es: " << total << endl;
+
+  return 0;
+
+}
