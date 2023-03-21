4535ac7382613bb97cdfbd42e7f272d5294663ef
Tomas Sosa <tomassosa.23@gmail.com>
2023-03-20 22:33:56 -0500

    Matriz

--- codes/Unidad1/punteros2.cxx
+++ codes/Unidad1/punteros2.cxx
@@ -18,8 +18,8 @@ void sumar(int *suma, int *v1, int *v2,int n){
         suma[i] = v1[i]+v2[i];
     }
 }
-int main(){
-    unsigned int num=10;
+int main(){ // Tanto p1, como p2, como p3 son arreglos, solo que p2 es estático, y p1 y p3 son dinámicos
+    unsigned int num=10;  // El unsigned es porque no puede ser negativo.
     int *p1 = new int[num]; // creo un arreglo de 10 enteros,
     int p2[] = {1,1,1,1,1,1,1,1,1,1}; // 10 enteros con asignación estática e inicializado 
                                       // con valores en 1, no necesita usarse delete, se borra solo
@@ -31,9 +31,21 @@ int main(){
     sumar(p3,p1,p2,num);
     print(p3,num);
 
-    delete[] p1;
+    delete[] p1;  // El [] es porque es un arreglo
     //delete[] p2;//no es necesario cuando es estática
     delete[] p3;
     
+    /*
+    El p2 no es necesario borrarlo ya que fue inicializado estaticamente, estos el complilador
+    los elimina solos
+    Si yo lo intentase borrar me saldrá un error.
+
+    Si yo creo un puntero dinámico dentro de una función y no lo elimino, cada que yo llame
+    la función estoy almacenando memoria en un puntero de manera innecesaria que nunca se 
+    elimina. En cambio, si el puntero hubiese sido declarado estáticamente este se elimina una 
+    vez finalice la funcion.
+
+    Buscar qué es el stack y qué es el heap
+    */
     return 0;
 }
